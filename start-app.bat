15fe41fe3262694da256f8e19a950f755a242bfa
adiletqorgau <sauleha035@gmail.com>
2026-05-29 00:06:28 +0500

    Add files via upload

--- start-app.bat
+++ start-app.bat
@@ -1,4 +1,4 @@
-const CACHE = "speakmate-v25";
+const CACHE = "speakmate-v26";
 const ASSETS = [
   "./",
   "./index.html",
@@ -55,7 +55,7 @@ self.addEventListener("notificationclick", (event) => {
           return client.focus();
         }
       }
-      return self.clients.openWindow("./?v=25");
+      return self.clients.openWindow("./?v=26");
     })
   );
 });
